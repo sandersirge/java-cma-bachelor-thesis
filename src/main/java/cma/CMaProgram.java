70479378e2e8a93ecfb1460a7bca459f78fb70f0
Simmo Saan <simmo.saan@gmail.com>
2022-02-02 13:52:05 +0200

    Paranda IntelliJ pisisoovitused

--- src/main/java/cma/CMaProgram.java
+++ src/main/java/cma/CMaProgram.java
@@ -2,12 +2,9 @@ package cma;
 
 import cma.instruction.CMaInstruction;
 
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.Collectors;
 
